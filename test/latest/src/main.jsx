42d6ef958d15c6f3a29293ef5f42d0e538e4dcef
Zyndesu <ericchansuiki@gmail.com>
2025-05-28 15:36:57 +0800

    feat: jsx

---
+++ test/latest/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HeaderApp from "./headerApp";
+
+ReactDOM.createRoot(document.getElementById("main-header")).render(
+  <React.StrictMode>
+    <HeaderApp />
+  </React.StrictMode>
+);
