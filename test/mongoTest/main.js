d21c308d4e6a3ffcf32f1e2d5f0a9cfa3ffcf339
Zyndesu <ericchansuiki@gmail.com>
2025-07-03 17:33:44 +0800

    feat: .env

--- test/mongoTest/main.js
+++ test/mongoTest/main.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ericchan:QUVqPnm4hvD3QaQp@cluster0.akkalj1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.KEY;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
